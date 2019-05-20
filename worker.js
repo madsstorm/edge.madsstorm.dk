bb5f55783325023f9693c9143142df1d5b651693
mads.hansen <mads.hansen@valtech.com>
2019-05-20 12:48:49 +0200

    More workers

--- worker.js
+++ worker.js
@@ -1,23 +1,21 @@
+/**
+ * Cloudflare workers implement the service worker spec
+ * See: https://developers.cloudflare.com/workers/about/ for an intro
+ *
+ * Binding an event handler to the fetch event allows your worker to intercept a request for your zone
+ */
 addEventListener('fetch', event => {
-    let response = handle(event.request);
-    response = setHeader(response);
-    response = setAnotherHeader(response);
-    event.respondWith(response);
+
+    // In the event of an uncaught exception, fail open as if the worker did not exist
+    event.passThroughOnException()
+   
+    event.respondWith(handle(event.request));
 })
   
 async function handle(request) {
     let response = await fetch(request);
-    return response;
-}
-
-function setHeader(response) {
     response = new Response(response.body, response);
     response.headers.set('Mads', 'Madsen2');
-    return response;
-}
-
-function setAnotherHeader(response) {
-    response = new Response(response.body, response);
     response.headers.set('Hans', 'Hansen5');
     return response;
 }
