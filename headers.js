6fa28af8e42789ebce942f97499a22e086790546
mads.hansen <mads.hansen@valtech.com>
2019-05-20 11:23:50 +0200

    More workers

--- headers.js
+++ headers.js
@@ -1,10 +1,23 @@
 addEventListener('fetch', event => {
-    event.respondWith(handle(event.request))
-  })
-  
-  async function handle(request) {
+    event.passThroughOnException();
+    event.respondWith(handle1(event.request));
+});
+
+addEventListener('fetch', event => {
+    event.passThroughOnException();
+    event.respondWith(handle2(event.request));
+});
+
+async function handle1(request) {
     let response = await fetch(request);
     response = new Response(response.body, response); 
     response.headers.set('Mads', 'Storm');  
     return response;
-  }
+};
+  
+async function handle2(request) {
+    let response = await fetch(request);
+    response = new Response(response.body, response); 
+    response.headers.set('Hansen', 'Hansen');
+    return response;
+};
--- welcome.js
+++ welcome.js
@@ -1,24 +1,26 @@
 addEventListener('fetch', event => {
 
-    let countryCode = event.request.headers.get('CF-IPCountry');
-  
-    let result = '<html><h1>The Edge</h1><span>';
-    
-    if(countryCode === 'DK') {
-      result = result + 'Velkommen!!';
-    } else if(countryCode === 'DE') {
-    result = result + 'Willkommen!!';
-    } else if(countryCode === 'ES') {
-    result = result + 'Hola!!';
-    } else {
-     result = result + 'Welcome!!'; 
-    }
-  
-    result = result + '</span></html>';
-  
-    const responseInit = {
-        headers: {'Content-Type': 'text/html'}
-      };
+  event.passThroughOnException();
 
-    event.respondWith(new Response(result, responseInit));
+  let countryCode = event.request.headers.get('CF-IPCountry');
+  
+  let result = '<html><h1>The Edge</h1><span>';
+    
+  if(countryCode === 'DK') {
+    result = result + 'Velkommen!!';
+  } else if(countryCode === 'DE') {
+  result = result + 'Willkommen!!';
+  } else if(countryCode === 'ES') {
+  result = result + 'Hola!!';
+  } else {
+    result = result + 'Welcome!!'; 
+  }
+
+  result = result + '</span></html>';
+
+  const responseInit = {
+      headers: {'Content-Type': 'text/html'}
+    };
+
+  event.respondWith(new Response(result, responseInit));
 });
