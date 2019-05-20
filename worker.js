f3f177bd3d21179a137a65ef9ca94ddc015b1197
mads.hansen <mads.hansen@valtech.com>
2019-05-20 16:01:49 +0200

    kv storage

--- worker.js
+++ worker.js
@@ -23,13 +23,15 @@ async function getBody(country) {
     const storageKey = "countryDetails-" + country;
 
     // Lookup country details in key value storage
-    let details = await kvStorage.get(storageKey)
-    if(details == null) {
+    let json = await kvStorage.get(storageKey)
+    if(json == null) {
         const response = await fetch('https://restcountries.eu/rest/v2/alpha/' + country)
         details = await response.json()
-        kvStorage.put(storageKey, details, { expirationTtl: 120 })
+        json = JSON.stringify(details)
+        kvStorage.put(storageKey, json, { expirationTtl: 60 })
     }
 
+    let details = JSON.parse(json);
     let body = '<a href="/"><div><img src="' + details.flag + '" style="width:100px;" /><span></div>' + details.nativeName + '</span></a>'
     return body
 }
