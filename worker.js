c3d6e429fb040e0f0e268d1446f8b72c7443e15f
mads.hansen <mads.hansen@valtech.com>
2019-05-20 12:24:29 +0200

    More workers

--- worker.js
+++ worker.js
@@ -1,10 +1,11 @@
 addEventListener('fetch', event => {
-    event.respondWith(handle(event.request))
+    let response = handle(event.request);
+    event.respondWith(response);
 })
   
 async function handle(request) {
-    let response = await fetch(request)
-    response = new Response(response.body, response)
+    let response = await fetch(request);
+    response = new Response(response.body, response);
     response.headers.set('Mads', 'Madsen');
-    return response
+    return response;
 }
