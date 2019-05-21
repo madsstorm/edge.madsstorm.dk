f2e48235b95fd1a6773f290e6ff687c352bca72a
mads.hansen <mads.hansen@valtech.com>
2019-05-21 17:16:02 +0200

    WIP: routing

--- worker.js
+++ worker.js
@@ -5,12 +5,18 @@ addEventListener('fetch', event => {
     const router = new Router();
     router.get('/', localizedHandler);
     router.get('/hello', helloHandler);
+    
+    router.get('/*', notFoundHandler);
 
     router.handleEvent(event)   
 })
 
 const helloHandler = async ({ request, params }) => {
     return new Response('Hello')
+}
+
+const notFoundHandler = async ({ request, params }) => {
+    return new Response('Page not found')
 }
 
 const localizedHandler = async ({ event }) => {
