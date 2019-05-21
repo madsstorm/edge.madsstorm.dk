0e259732a01415b81d6c5db7d85a5e3e6c64f0ed
mads.hansen <mads.hansen@valtech.com>
2019-05-21 16:54:46 +0200

    WIP: routing

--- worker.js
+++ worker.js
@@ -1,12 +1,19 @@
 import { Router } from 'service-worker-router'
 import { getLocalBody } from './localContent'
 
+const router = new Router();
+router.get('/', littleHandler);
+
 addEventListener('fetch', event => {
     event.passThroughOnException();
-
-    event.respondWith(handle(event));
+    router.handleEvent(event)   
+    // event.respondWith(handle(event));
 })
-  
+
+const littleHandler = async ({ request, params }) => {
+    return new Response('Hello')
+}
+
 async function handle(event) {
     const body = await getLocalBody(event);
     const responseInit = { headers: {'content-type':'text/html; charset=UTF-8'} };
