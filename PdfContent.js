a8cb46d82a173970d8059de3bbcecea3985695fe
madsstorm <madsstorm@gmail.com>
2019-05-22 23:40:08 +0200

    remove pdf

--- PdfContent.js
+++
@@ -1,7 +0,0 @@
-export class PdfContent {
-    async CreateResponse({event}) {
-
-        const responseInit = { headers: {'content-type':'application/pdf'} };
-        return new Response('pdf', responseInit);
-    }
-}
--- worker.js
+++ worker.js
@@ -1,14 +1,11 @@
 import { Router } from 'service-worker-router'
 import { LocalizedContent } from './LocalizedContent'
-import { PdfContent } from './PdfContent'
 
 addEventListener('fetch', event => {
     const localized = new LocalizedContent();
-    const pdf = new PdfContent();
 
     const router = new Router();   
     router.get('/', localized.CreateResponse);
-    router.get('/pdf', pdf.CreateResponse);
     router.get('/*', notFoundHandler);
 
     router.handleEvent(event);
