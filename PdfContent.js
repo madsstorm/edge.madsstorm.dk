3a63f796524c6ea0df999a6abeca2117770df868
madsstorm <madsstorm@gmail.com>
2019-05-22 23:28:32 +0200

    wip: pdf

--- PdfContent.js
+++ PdfContent.js
@@ -1,5 +1,9 @@
+const PDFDocument = require('pdfkit');
+
 export class PdfContent {
     async CreateResponse({event}) {
+
+        const doc = new PDFDocument;
 
         const responseInit = { headers: {'content-type':'application/pdf'} };
         return new Response('pdf', responseInit);
