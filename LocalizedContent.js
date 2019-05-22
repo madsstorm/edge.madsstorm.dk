3a5fcab9e3d30126f37042ac86dbaee01567d652
mads.hansen <mads.hansen@valtech.com>
2019-05-22 12:27:24 +0200

    IATA datacenter codes

--- LocalizedContent.js
+++ LocalizedContent.js
@@ -22,7 +22,7 @@ export class LocalizedContent {
 
         if(!datacenterName) {
 
-            return new Response('checkpoint-' + datacenterCode);
+            return new Response(datacenterCode);
             // return new Response(dataCenterCode);
 
             // return new Response('&code=' + dataCenterCode);
