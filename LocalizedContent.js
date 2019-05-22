f5d3c1d1b2df5ee32d6433295680c539698340af
mads.hansen <mads.hansen@valtech.com>
2019-05-22 10:54:51 +0200

    IATA datacenter codes

--- LocalizedContent.js
+++ LocalizedContent.js
@@ -22,8 +22,8 @@ export class LocalizedContent {
 
         if(!datacenterName) {
 
-
-return new Response('https://iatacodes.org/api/v6/airports?api_key=' + iataCodesApiKey + '&code=' + dataCenterCode);
+            return new Response('https://iatacodes.org/api/v6/airports?api_key=' + iataCodesApiKey);
+// return new Response('https://iatacodes.org/api/v6/airports?api_key=' + iataCodesApiKey + '&code=' + dataCenterCode);
 
 
             // "Expensive" external call that we want to cache in KV
