aa5bbd491d4eb23e7b062fe7a12e06c81d6cc378
mads.hansen <mads.hansen@valtech.com>
2019-05-24 15:34:19 +0200

    Remove sentry

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
 import { Router } from 'service-worker-router'
 import { LocalizedContent } from './LocalizedContent'
-import { SentryLogger } from './SentryLogger'
 
 addEventListener('fetch', event => {
     const localized = new LocalizedContent();
@@ -13,7 +12,5 @@ addEventListener('fetch', event => {
 })
 
 const notFoundHandler = async ({ request, params }) => {
-    const logger = new SentryLogger();
-     await logger.promisifiedSentryLog('Page not found');   
     return new Response('Page not found :o(');
 }
